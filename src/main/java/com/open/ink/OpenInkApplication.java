d19b001b0ceae7c359272b0862709e60ca475989
batoula1 <batoulaa52gmail.com>
2024-09-12 20:14:08 +0600

    added base project

---
+++ src/main/java/com/open/ink/OpenInkApplication.java
@@ -0,0 +1,13 @@
+package com.open.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenInkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenInkApplication.class, args);
+    }
+
+}
