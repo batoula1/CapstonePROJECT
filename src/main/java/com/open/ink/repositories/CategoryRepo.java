03036e5b8a67fd5908db2eeae7380d1c962ff152
batoula1 <batoulaa52gmail.com>
2024-09-12 20:25:40 +0600

    added entities and repo

--- src/main/java/com/open/ink/repositories/CategoryRepo.java
+++ src/main/java/com/open/ink/repositories/CategoryRepo.java
@@ -2,9 +2,8 @@ package com.open.ink.repositories;
 
 import java.util.List;
 
+import com.open.ink.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.onneshon.blog.entities.Category;
 
 public interface CategoryRepo extends JpaRepository<Category, Integer> {
 	List<Category> findByCategoryTitle(String title);
